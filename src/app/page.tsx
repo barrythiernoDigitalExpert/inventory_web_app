ce52eb390e726b7bcb43044d4604b792f78e1693
BARRY Thierno Alhassane <thiernoalhassanebarry6@gmail.com>
2025-05-08 08:21:36 +0000

    Rectify Redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
   // Redirection vers la page de connexion
-  redirect('/auth/login');
+  redirect('/login');
   
   // Cette partie ne sera jamais exécutée en raison de la redirection
   return null;
