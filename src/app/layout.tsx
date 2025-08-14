4d2823cd7244a53af5dcf8bc258bae95453c0be6
BARRY Thierno Alhassane <thiernoalhassanebarry6@gmail.com>
2025-08-14 16:04:56 +0000

    add speedtest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SessionProvider } from 'next-auth/react';
 import { AuthProvider } from '@/lib/context/AuthContext';
 import './globals.css';
@@ -27,6 +28,7 @@ export default function RootLayout({
             {children}
           </AuthProvider>
         </Providers>
+         <SpeedInsights />
       </body>
     </html>
   );
